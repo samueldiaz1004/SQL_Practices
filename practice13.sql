ddd633bcc9e147baa2f02d6f4b7538841fb8708f
Samuel Díaz <71342016+samueldiaz1004@users.noreply.github.com>
2022-05-16 08:45:23 -0500

    Update practice13.sql

--- practice13.sql
+++ practice13.sql
@@ -1,2 +1,200 @@
 -- Practice 13 - Functions and procedures
 -- Samuel Alejandro Díaz del Guante Ochoa
+
+-- Function: is a common language runtime which accepts the parameters, 
+-- and these perform an action, like as advanced complex calculations, 
+-- which return the result of the action as a value.
+
+-- A function can use other programming languages to define an action (Ex. python for Postgres).
+
+-- Trigger: is a statement that a system executes automatically when there is 
+-- any modification to the database. In a trigger, we first specify when the 
+-- trigger is to be executed and then the action to be performed when the trigger executes.
+
+-- Triggers can be defined in diferent types (Ex. after insert, after update, after delete, before insert, etc.).
+
+-- Create a percent_change function
+CREATE OR REPLACE FUNCTION
+percent_change(new_value numeric,
+               old_value numeric,
+               decimal_places integer DEFAULT 1)
+RETURNS numeric AS
+'SELECT round(
+       ((new_value - old_value) / old_value) * 100, decimal_places
+);'
+LANGUAGE SQL
+IMMUTABLE
+RETURNS NULL ON NULL INPUT;
+
+-- Use function
+SELECT percent_change(110, 108, 2);
+
+-- Delete function
+DROP FUNCTION percent_change(numeric,numeric,integer);
+
+-- Add a column to a table and update personal days column
+ALTER TABLE teachers ADD COLUMN personal_days integer;
+
+CREATE OR REPLACE PROCEDURE update_personal_days()
+AS $$
+BEGIN
+    UPDATE teachers
+    SET personal_days =
+        CASE WHEN (now() - hire_date) >= '10 years'::interval
+                  AND (now() - hire_date) < '15 years'::interval THEN 4
+             WHEN (now() - hire_date) >= '15 years'::interval
+                  AND (now() - hire_date) < '20 years'::interval THEN 5
+             WHEN (now() - hire_date) >= '20 years'::interval
+                  AND (now() - hire_date) < '25 years'::interval THEN 6
+             WHEN (now() - hire_date) >= '25 years'::interval THEN 7
+             ELSE 3
+        END;
+    RAISE NOTICE 'personal_days updated!';
+END;
+$$
+LANGUAGE plpgsql;
+
+-- Invoke the procedure:
+CALL update_personal_days();
+
+-- Enable the PL/Python procedural language
+CREATE EXTENSION plpython3u;
+
+-- Use python to create a trim_county() function
+CREATE OR REPLACE FUNCTION trim_county(input_string text)
+RETURNS text AS $$
+    import re
+    cleaned = re.sub(r' County', '', input_string)
+    return cleaned
+$$
+LANGUAGE plpython3u;
+
+-- Test trim_country() function
+SELECT county_name,
+       trim_county(county_name)
+FROM us_counties_pop_est_2019
+ORDER BY state_fips, county_fips
+LIMIT 5;
+
+-- Create tables to test triggers
+CREATE TABLE grades (
+    student_id bigint,
+    course_id bigint,
+    course text NOT NULL,
+    grade text NOT NULL,
+PRIMARY KEY (student_id, course_id)
+);
+
+INSERT INTO grades
+VALUES
+    (1, 1, 'Biology 2', 'F'),
+    (1, 2, 'English 11B', 'D'),
+    (1, 3, 'World History 11B', 'C'),
+    (1, 4, 'Trig 2', 'B');
+
+CREATE TABLE grades_history (
+    student_id bigint NOT NULL,
+    course_id bigint NOT NULL,
+    change_time timestamp with time zone NOT NULL,
+    course text NOT NULL,
+    old_grade text NOT NULL,
+    new_grade text NOT NULL,
+PRIMARY KEY (student_id, course_id, change_time)
+); 
+
+-- Create record_if_grade_changed() function
+CREATE OR REPLACE FUNCTION record_if_grade_changed()
+    RETURNS trigger AS
+$$
+BEGIN
+    IF NEW.grade <> OLD.grade THEN
+    INSERT INTO grades_history (
+        student_id,
+        course_id,
+        change_time,
+        course,
+        old_grade,
+        new_grade)
+    VALUES
+        (OLD.student_id,
+         OLD.course_id,
+         now(),
+         OLD.course,
+         OLD.grade,
+         NEW.grade);
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create trigger
+CREATE TRIGGER grades_update
+  AFTER UPDATE
+  ON grades
+  FOR EACH ROW
+  EXECUTE PROCEDURE record_if_grade_changed();
+  
+-- Update grade
+UPDATE grades
+SET grade = 'C'
+WHERE student_id = 1 AND course_id = 1;
+
+-- Check results from history
+SELECT student_id,
+       change_time,
+       course,
+       old_grade,
+       new_grade
+FROM grades_history;
+
+-- Create another table to test functions and triggers
+CREATE TABLE temperature_test (
+    station_name text,
+    observation_date date,
+    max_temp integer,
+    min_temp integer,
+    max_temp_group text,
+PRIMARY KEY (station_name, observation_date)
+);
+
+-- Create function
+CREATE OR REPLACE FUNCTION classify_max_temp()
+    RETURNS trigger AS
+$$
+BEGIN
+    CASE
+       WHEN NEW.max_temp >= 90 THEN
+           NEW.max_temp_group := 'Hot';
+       WHEN NEW.max_temp >= 70 AND NEW.max_temp < 90 THEN
+           NEW.max_temp_group := 'Warm';
+       WHEN NEW.max_temp >= 50 AND NEW.max_temp < 70 THEN
+           NEW.max_temp_group := 'Pleasant';
+       WHEN NEW.max_temp >= 33 AND NEW.max_temp < 50 THEN
+           NEW.max_temp_group := 'Cold';
+       WHEN NEW.max_temp >= 20 AND NEW.max_temp < 33 THEN
+           NEW.max_temp_group := 'Frigid';
+       WHEN NEW.max_temp < 20 THEN
+           NEW.max_temp_group := 'Inhumane';    
+       ELSE NEW.max_temp_group := 'No reading';
+    END CASE;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Create trigger
+CREATE TRIGGER temperature_insert
+    BEFORE INSERT
+    ON temperature_test
+    FOR EACH ROW
+    EXECUTE PROCEDURE classify_max_temp();
+    
+-- Insert new values to table to test the update temperature_update trigger
+INSERT INTO temperature_test
+VALUES
+    ('North Station', '1/19/2023', 10, -3),
+    ('North Station', '3/20/2023', 28, 19),
+    ('North Station', '5/2/2023', 65, 42),
+    ('North Station', '8/9/2023', 93, 74),
+    ('North Station', '12/14/2023', NULL, NULL);
+    
+SELECT * FROM temperature_test ORDER BY observation_date;
